83963bd56b271927ca96d5570191a5c75afa7807
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2024-04-21 16:41:17 +0100

    Adds custom error response class

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const ErrorResponse = require('../utils/errorResponse');
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc    Get all bootcamps
@@ -24,7 +25,7 @@ exports.getBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.findById(req.params.id);
 
         if(!bootcamp) {
-            return res.status(400).json({ success: false });
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
         }
 
         res.status(200).json({
@@ -32,8 +33,7 @@ exports.getBootcamp = async (req, res, next) => {
             data: bootcamp
         });
     } catch (error) {
-        // res.status(400).json({ success: false });
-        next(error)
+        next(error);
     }
 }
 
@@ -49,7 +49,7 @@ exports.createBootcamp = async (req, res, next) => {
             data: bootcamp
         });
     } catch (error) {
-        res.status(400).json({ success: false });
+        next(error);
     }
     
 }
@@ -65,7 +65,7 @@ exports.updateBootcamp = async (req, res, next) => {
         });
 
         if(!bootcamp) {
-            return res.status(400).json({ success: false });
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
         }
 
         res.status(200).json({
@@ -73,7 +73,7 @@ exports.updateBootcamp = async (req, res, next) => {
             data: bootcamp
         })
     } catch (error) {
-        res.status(400).json({ success: false });
+        next(error);
     }
     
 }
@@ -86,7 +86,7 @@ exports.deleteBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
         if(!bootcamp) {
-            return res.status(400).json({ success: false });
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
         }
 
         res.status(200).json({
@@ -94,6 +94,6 @@ exports.deleteBootcamp = async (req, res, next) => {
             data: {}
         })
     } catch (error) {
-        res.status(400).json({ success: false });
+        next(error);
     }
 }
