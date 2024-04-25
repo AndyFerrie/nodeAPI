26051bc4e2fbeac1d09bcf876875cf0d6504f623
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2024-04-25 18:20:25 +0100

    Adds functionality to get courses for bootcamp

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,28 +1,30 @@
-const express = require('express');
-const { 
-    getBootcamps, 
-    getBootcamp, 
-    createBootcamp, 
-    updateBootcamp, 
-    deleteBootcamp,
-    getBootcampsInRadius
-} = require('../controllers/bootcamps');
+const express = require("express");
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+  getBootcampsInRadius,
+} = require("../controllers/bootcamps");
+
+// Include other resource routers
+
+const courseRouter = require("./courses");
 
 const router = express.Router();
 
-router
-    .route('/radius/:zipcode/:distance')
-    .get(getBootcampsInRadius)
+// Re-route into other resource routers
+router.use("/:bootcampId/courses", courseRouter);
+
+router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
+
+router.route("/").get(getBootcamps).post(createBootcamp);
 
 router
-    .route('/')
-    .get(getBootcamps)
-    .post(createBootcamp);
-
-router
-    .route('/:id')
-    .get(getBootcamp)
-    .put(updateBootcamp)
-    .delete(deleteBootcamp);
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
